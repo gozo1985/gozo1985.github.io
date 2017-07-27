5bbf209ff24d571f3cc07c242ac5f23f792013f7
Christoph Flasch <flasch@super-fi.eu>
2017-07-27 16:27:03 +0200

    subnavi function

--- htdocs/javascripts/main.js
+++ htdocs/javascripts/main.js
@@ -1,5 +1,7 @@
 $(function(){
 
+
+//jQuery function for checking if an element is in viewport or not
 $.fn.isInViewport = function(toleranceTop = 0, toleranceBottom = 0, toleranceLeft = 0, toleranceRight = 0){
 
     var win = $(window);
@@ -18,6 +20,8 @@ $.fn.isInViewport = function(toleranceTop = 0, toleranceBottom = 0, toleranceLef
     return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
 };
 
+
+// Responsive breakpoints, data-attributes on body and global scope vars
 var responsive = {
 	init: function() {
 		var win = $(window);
@@ -25,7 +29,7 @@ var responsive = {
 		var viewportWidth = win.outerWidth();
 		window.device = 'desktop';
 	
-		win.resize(function(){
+		win.on('resize scroll', function(){
 			var viewportWidth = win.outerWidth();
 		
 			switch(true) {
@@ -43,14 +47,13 @@ var responsive = {
 					window.device = 'desktop';
 					body.attr('data-device', 'desktop');
 			}
-			body.scroll();
-		});
-		body.resize();
+		}).resize().scroll();
 	}
 };
 responsive.init();
 
 
+// jQuery slick plugin for a simple carousel (selector: '.carousel')
 var carousel = {
 	init: function() {
 		var carousel = $('.carousel');
@@ -78,30 +81,32 @@ var carousel = {
 carousel.init();
 
 
+// Sections fade in/out when in viewport. Comment out init function call to remove effect. (selector: 'section.content')
 var sectionsFadeIn = {
 	init: function() {
 		var win = $(window);
-		var body = $('body');
 
 		$('.page section.content').each(function(index, element){
 			var e = $(element);
 
 			e.addClass('opacity-0');
 
-			win.scroll(function(){
+			win.on('resize scroll', function(){
 				if(e.isInViewport()) {
 					e.addClass('in-view');
+					e.removeClass('opacity-0');
 				} else {
 					e.removeClass('in-view');
+					e.addClass('opacity-0');
 				}
-			});
+			}).resize().scroll();
 		});
-		body.scroll();
 	}
 };
 sectionsFadeIn.init();
 
 
+// Main navigation with sub sticky, responsive with burger icon on smartphones
 var mainNav = {
 	nav: $('nav#main'),
 	navList: $('nav#main').find('ul').first(),
@@ -112,7 +117,7 @@ var mainNav = {
 		var isSticky = false;
 		var toleranceTop = nav.outerHeight() * (-1);
 
-		win.scroll(function(){
+		win.on('resize scroll', function(){
 			var navNextElement = nav.next();
 			var navNextElementBounds = navNextElement.offset();
 			var navInitPosTop = navNextElementBounds.top + toleranceTop;
@@ -132,21 +137,54 @@ var mainNav = {
 	phone: () => {
 		var win = $(window);
 		var iconMenu = mainNav.nav.find('div.icon-menu');
-		var navMenuSlideDuaration = 300;
+		var navSlideDuaration = 300;
 
 		iconMenu.on('click.icon-menu',function(){
-			mainNav.navList.slideToggle(navMenuSlideDuaration);
+			mainNav.navList.slideToggle(navSlideDuaration);
 		});
 
-		win.resize(function(){
+		win.on('resize scroll', function(){
 			(window.device !== 'phone') ? mainNav.navList.removeAttr('style') : false;
 		});
 	},
+
+	subnav: () => {
+		var subnav = mainNav.nav.find('#sub');
+		var mainNavLinks = mainNav.nav.find('a');
+		var subnavSlideDuaration = 300;
+		var iconUp = subnav.find('.icon-circle-up');
+
+		subnav.addClass('hidden');
+
+		mainNavLinks.each(function(index, element){
+			var e = $(element);
+
+			e.on('click.subnav', function(event){
+				event.preventDefault();
+
+				subnav.slideDown(subnavSlideDuaration, function(){
+					subnav.removeClass('hidden').addClass('box-shadow');
+					iconUp.show();
+				});
+			});
+		});
+
+		iconUp.on('click.close', function(event){
+			
+			iconUp.hide();
+			
+			subnav.slideUp(subnavSlideDuaration, function(){
+				subnav.addClass('hidden').removeClass('box-shadow');
+			});
+		});
+	}
 }
 mainNav.sticky();
 mainNav.phone();
+mainNav.subnav();
 
 
+// Sections with respnsive video as background (selector: 'section.video-bg')
 var videoBg = {
 	init: function() {
 		var win = $(window);
@@ -154,19 +192,22 @@ var videoBg = {
 		var videoWrapper =  videoSection.find('.video-wrapper');
 		var video = videoWrapper.find('video');
 
-		video.bind('loadeddata', function(event) {
-  			win.resize(function(){
-				
-				if (window.device !== 'desktop') {
-					videoSection.height(video.css('min-height'));
-					videoMargin = (win.width() - video.outerWidth()) / 2;
-					video.css('margin-left', videoMargin);
-				} else {
-					videoSection.height(video.outerHeight());
-					video.css('margin-left', 0);
-				}
-			});
-			win.resize();
+		var videoResponsive = function() {
+			if (window.device !== 'desktop') {
+				videoSection.height(video.css('min-height'));
+				videoMargin = (win.width() - video.outerWidth()) / 2;
+				video.css('margin-left', videoMargin);
+			} else {
+				videoSection.height(video.outerHeight());
+				video.css('margin-left', 0);
+			}
+		};
+
+		video.bind('load resize', function(){
+			videoResponsive();
+		});
+		win.on('resize scroll', function(){
+			videoResponsive();
 		});
 	}
 }
